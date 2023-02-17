8b14de036fe4f1ba318495fe857fe7c9857f1bb1
Ben-Drucker <ben.drucker@icloud.com>
2023-02-16 16:35:14 -0800

    change discovery script

--- discovery/run_main_api.sh
+++ discovery/run_main_api.sh
@@ -5,5 +5,5 @@ then
     echo "Failed: please run this script from DeepKS/discovery/";
 else
     cd /root/ML/DeepKS_/
-    stdbuf -o0 python3 -m trace --ignore-dir /opt/conda/envs/py310/lib --trace --module DeepKS.api.main --scores -p in_order_json --cartesian-product --device cuda:4 -kf discovery/kinase_list_small.txt -sf discovery/site_list_small.txt
+    stdbuf -o0 python3 -m trace --ignore-dir /opt/conda/envs/py310/lib --trace --module DeepKS.api.main --scores -p in_order_json --cartesian-product --device cuda:7 -kf discovery/kinase_list_small.txt -sf discovery/site_list_small.txt
 fi
