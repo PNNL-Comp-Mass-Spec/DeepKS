8869e881b90c3280c3917ee956c808280e23fa5c
Ben Drucker <ben.drucker@icloud.com>
2023-03-02 21:29:17 -0500

    Fixed missing import; added explanation to tree_description

--- data/preprocessing/PreprocessingSteps/__init__.py
+++ data/preprocessing/PreprocessingSteps/__init__.py
@@ -1,4 +1,3 @@
-from . import format_raw_data_DD_group_classifier
 from . import format_raw_data_DD
 from . import get_kin_fam_grp
 from ....tools import make_fasta
@@ -6,4 +5,4 @@ from . import remove_overlaps
 from . import split_into_sets_individual_deterministic_top_k
 from . import download_psp
 
-__all__ = ["format_raw_data_DD_group_classifier", "format_raw_data_DD", "get_kin_fam_grp", "make_fasta", "remove_overlaps", "split_into_sets_individual_deterministic_top_k", "download_psp"]
+__all__ = ["format_raw_data_DD", "get_kin_fam_grp", "make_fasta", "remove_overlaps", "split_into_sets_individual_deterministic_top_k", "download_psp"]
