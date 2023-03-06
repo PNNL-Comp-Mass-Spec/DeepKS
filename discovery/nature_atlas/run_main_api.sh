7c1a0a90ac5964f59c261c7e745eea44f4f3e781
Ben Drucker <ben.drucker@icloud.com>
2023-03-06 13:24:30 -0500

    Fixed partition size stuff

--- discovery/nature_atlas/run_main_api.sh
+++ discovery/nature_atlas/run_main_api.sh
@@ -7,10 +7,5 @@ else
     KIN_SIZE=10
     SITE_SIZE=89784
     cd ../../../
-    python3 -m DeepKS.api.main --suppress-seqs-in-output --scores --normalize --groups \
-    -p csv --cartesian-product --device cpu \
-    -kf discovery/nature_atlas/kinase_list_$KIN_SIZE.txt -sf discovery/nature_atlas/site_list_$SITE_SIZE.txt \
-    --kin-info discovery/nature_atlas/compact_kinase_info_$KIN_SIZE.json \
-    --site-info discovery/nature_atlas/compact_site_info_86201.json \
-    --bypass-group-classifier
+    python3 -m DeepKS.api.main --suppress-seqs-in-output --scores --normalize --groups -p csv --cartesian-product --device cpu -kf discovery/nature_atlas/kinase_list_$KIN_SIZE.txt -sf discovery/nature_atlas/site_list_$SITE_SIZE.txt --kin-info discovery/nature_atlas/compact_kinase_info_$KIN_SIZE.json --site-info discovery/nature_atlas/compact_site_info_86201.json --bypass-group-classifier
 fi
